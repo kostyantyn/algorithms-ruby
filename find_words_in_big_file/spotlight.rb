d1f80d76d318897c744e2491690c2cddbaae7382
Kostyantyn Stepanyuk <kostyantyn.stepanyuk@onapp.com>
2012-01-27 19:21:51 +0200

    optimize algorithm. Don't check line if it has wrong size.

--- find_words_in_big_file/spotlight.rb
+++ find_words_in_big_file/spotlight.rb
@@ -2,8 +2,9 @@
 
 class Spotlight
   def initialize(word, file)
-    @file = file
-    @word = parse(word)
+    @file  = file
+    @word  = word
+    @chars = parse(word)
   end
   
   def parse(word)
@@ -18,13 +19,16 @@ class Spotlight
     map    = {}
     File.open(@file, 'r') do |io|
       io.each_line do |line|
+        next if line.size > @word.size
+        next if result[9] && line.size <= result[9].size
         map.clear
         line.each_char do |char|
-          if @word.has_key?(char)
+          if @chars.has_key?(char)
             map[char] = map.has_key?(char) ? map[char] + 1 : 1
-            break if map[char] > @word[char]
+            break if map[char] > @chars[char]
           elsif char == "\n"
             result << line.chomp
+            result.sort! { |a, b| b.length <=> a.length }
           else
             break
           end
@@ -35,4 +39,4 @@ class Spotlight
   end
 end
 
-p Spotlight.new('австралопітек', 'vocabulary.txt').seek.sort { |a, b| b.length <=> a.length }.first(10)
+p Spotlight.new('австралопітек', 'vocabulary.txt').seek.first(10)
